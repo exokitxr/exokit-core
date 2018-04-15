5e577b2583446bde9d2acd38059c00b404fb71f1
Avaer Kazmer <a@modules.io>
2018-04-14 21:21:41 -0400

    Revert "URLs use http:// by default"
    This reverts commit af70d9ca816cbc7648b1e08f0425f9772410861e.

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ class Location extends EventEmitter {
   }
   // triggers navigation
   get href() { return this._url.href; }
-  set href(href) { this.set(href); this.update(); }
+  set href(href) { this._url.href = href; this.update(); }
   get protocol() { return this._url.protocol; }
   set protocol(protocol) { this._url.protocol = protocol; this.update(); }
   get host() { return this._url.host; }
@@ -102,9 +102,6 @@ class Location extends EventEmitter {
   set origin(origin) {} // read only
   // helpers
   set(u) {
-    if (u.indexOf('://') < 0) {
-      u = 'http://' + u;
-    }
     this._url.href = u;
   }
   update() {
